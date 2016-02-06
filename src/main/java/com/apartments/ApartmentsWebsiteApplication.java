f5e674c3abb1101f9c6eede8a26e23771f7eecb8
Petar Butkovic <petar.butkovic@gmail.com>
2016-02-06 19:06:48 +0100

    Initial Commit

---
+++ src/main/java/com/apartments/ApartmentsWebsiteApplication.java
@@ -0,0 +1,12 @@
+package com.apartments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApartmentsWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApartmentsWebsiteApplication.class, args);
+	}
+}
