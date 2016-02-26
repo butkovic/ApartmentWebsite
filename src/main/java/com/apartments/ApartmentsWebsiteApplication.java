35fa9f6e1c367ad255baac06f64b024785a4dd51
Petar Butkovic <petar.butkovic@gmail.com>
2016-02-26 20:46:24 +0100

    Add internationalization.

--- src/main/java/com/apartments/ApartmentsWebsiteApplication.java
+++ src/main/java/com/apartments/ApartmentsWebsiteApplication.java
@@ -2,11 +2,36 @@ package com.apartments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @SpringBootApplication
-public class ApartmentsWebsiteApplication {
+public class ApartmentsWebsiteApplication extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApartmentsWebsiteApplication.class, args);
 	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		return new CookieLocaleResolver();
+	}
+
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+		lci.setParamName("lang");
+		return lci;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localeChangeInterceptor());
+	}
+
 }
+
