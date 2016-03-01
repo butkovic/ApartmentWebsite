16449bc751bbbac6a702ac113c3085f7e21cae38
Danijel Butković <danijel.butkovic@hotmail.com>
2016-03-01 20:20:05 +0100

    ...

--- src/main/java/com/apartments/website/controllers/MainController.java
+++ src/main/java/com/apartments/website/controllers/MainController.java
@@ -3,13 +3,15 @@ package com.apartments.website.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
 /**
  * Created by Petar Butkovic on 07/02/2016. controller
  */
 @Controller
 @RequestMapping("/")
 public class MainController {
+    int room = 0;
 
     @RequestMapping(method = RequestMethod.GET)
     public String getIndexView(){
@@ -54,9 +56,32 @@ public class MainController {
         return "gallery";
     }
 
+    /*@RequestMapping(value = "/property-detail-1", method = RequestMethod.GET)
+    public String getPropertyDetail1View(){ return "property-detail-1"; }
+    */
     @RequestMapping(value = "/property-detail-1", method = RequestMethod.GET)
-    public String getPropertyDetail1View(){
+    public String getRoomDetail1View(@RequestParam(value="room", required=false, defaultValue="1") String room, Model model) {
+        model.addAttribute("room", room);
         return "property-detail-1";
     }
-
+    @RequestMapping(value = "/property-detail-2", method = RequestMethod.GET)
+    public String getRoomDetail2View(@RequestParam(value="room", required=false, defaultValue="2") String room, Model model) {
+        model.addAttribute("room", room);
+        return "property-detail-1";
+    }
+    @RequestMapping(value = "/property-detail-3", method = RequestMethod.GET)
+    public String getRoomDetail3View(@RequestParam(value="room", required=false, defaultValue="3") String room, Model model) {
+        model.addAttribute("room", room);
+        return "property-detail-1";
+    }
+    @RequestMapping(value = "/property-detail-4", method = RequestMethod.GET)
+    public String getRoomDetail4View(@RequestParam(value="room", required=false, defaultValue="4") String room, Model model) {
+        model.addAttribute("room", room);
+        return "property-detail-1";
+    }
+    @RequestMapping(value = "/property-detail-5", method = RequestMethod.GET)
+    public String getRoomDetail5View(@RequestParam(value="room", required=false, defaultValue="5") String room, Model model) {
+        model.addAttribute("room", room);
+        return "property-detail-1";
+    }
 }
